4c5a3ae4d3a2e30fe25551f1673f6ebf4b7ffd54
Joost Peters <48448615+JoostPeters106@users.noreply.github.com>
2025-11-25 16:26:10 +0100

    fix new version of express

--- server.js
+++ server.js
@@ -213,7 +213,7 @@ app.post('/api/agent1/revisit', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
